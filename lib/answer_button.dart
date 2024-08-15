eff304557521d9a2b82deb908fd9d91f6573860f
Faisal Rahman <qyupaww@gmail.com>
2024-08-15 18:19:49 +0700

    button padding

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -13,6 +13,15 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+        ),
         foregroundColor: primaryColor,
       ),
       child: Text(answerText),
